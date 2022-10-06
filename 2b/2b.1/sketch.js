c1f60669475429e9f91105cc08db6b6f69f46e87
Hoda <hoda.afssari@sjsu.edu>
2022-10-06 14:43:13 -0700

    adding randomizer 2b.4

---
+++ 2b/2b.1/sketch.js
@@ -0,0 +1,14 @@
+//My favorite cats
+let cats = ["Bella", "Simba", "Zuko", "Cookie", "Meowmeow", "Meowmeow's triplets" ];
+
+function setup () {
+    createCanvas (600,600);
+    background (114);
+
+   
+
+}
+
+function draw () { 
+
+}   
---
+++ 2b/2b.4/sketch.js
@@ -0,0 +1,33 @@
+//My favorite cats
+let cats = [{
+    name: "Bella", 
+    owner: "Alizeh"
+}, {
+    name: "Zuko", 
+    owner: "Sareh"
+}, {
+    name: "Whisker", 
+owner: "Sareh"
+}, {
+    name: "Cookie", 
+    owner: "Maryam"
+}, {
+    name: "Meowmeow" , 
+    owner: "Afssari"
+}, {
+    name: "Meowmeow's triplets", 
+    owner: "Afssari" 
+}];
+
+
+function setup () {
+    createCanvas (600,600);
+    background (114);
+
+    console.log(random(5));
+
+}
+
+function draw () { 
+
+}    
