9f77618b7c38b7081c9e30cd94a77a952ad39776
Hoda <hoda.afssari@sjsu.edu>
2022-10-06 21:44:25 -0700

    finished 2b.4, working on 2b.5

--- 2b/2b.4/sketch.js
+++ 2b/2b.4/sketch.js
@@ -1,4 +1,8 @@
-//My favorite cats
+//Cats I love throughout my years and their owners 
+
+
+//array
+//global things
 let cats = [{
     name: "Bella", 
     owner: "Alizeh"
@@ -7,7 +11,7 @@ let cats = [{
     owner: "Sareh"
 }, {
     name: "Whisker", 
-owner: "Sareh"
+    owner: "Sareh"
 }, {
     name: "Cookie", 
     owner: "Maryam"
@@ -19,15 +23,35 @@ owner: "Sareh"
     owner: "Afssari" 
 }];
 
+//calling variable
+let randomIndex;
 
+//runs once and stays
 function setup () {
-    createCanvas (600,600);
-    background (114);
-
-    console.log(random(5));
+    createCanvas (windowWidth,windowHeight);
+    background (168,250,240);
 
 }
 
 function draw () { 
 
+    //saying this so user knows to click to start randomizer
+    text("click to reveal a random cat name from a selection, up to 6 clicks allowed before having to refresh (for now)",windowWidth/2.3,windowHeight/2.3);mousePressed () 
+
+let mousePressed
 }    
+
+function mousePressed () {
+
+    //background restated so that the array text doesn't stack up on each other and "erases" it
+background(168,250,240);
+
+
+
+    // defining variable: int makes it a whole number, random(cats.length) chooses a random number within the array
+randomIndex = int(random(cats.length));
+   //writes out the random number, .key is what category it chooses, and then at what location on screen
+  text(cats[randomIndex].name,windowWidth/2,windowHeight/2);
+  //removes chosen number from array in this instance
+  cats.splice(random.Index,1);
+}
