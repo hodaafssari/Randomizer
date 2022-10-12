528f2f0d7e20a656b91548375d07e2e9bee4e648
Hoda <hoda.afssari@sjsu.edu>
2022-10-12 00:13:56 -0700

    updating 2b

--- 2b/2b.7/sketch.js
+++ 2b/2b.7/sketch.js
@@ -25,6 +25,9 @@ let cats = [{
 
 //calling variable
 let randomIndex;
+ let animating = true; 
+
+ 
 
 //runs once and stays
 function setup () {
@@ -32,6 +35,7 @@ function setup () {
     background (168,250,240);
     textSize (windowWidth/25);
 
+//WHERE DO I CALL IT??????!!!!let mousePressed; 
     
 }
 
@@ -39,20 +43,21 @@ function draw () {
 
     //saying this so user knows to click to start randomizer
 textAlign (CENTER);
-text ('click to reveal a random cat name from a selection',windowWidth/2,windowHeight/2);mousePressed () 
+text ('click to reveal a random cat name/owner name from a selection',windowWidth/2,windowHeight/2);mousePressed () 
+
+  if(animating == true){
+    ellipse (random(width), random(height), random(windowWidth/5, windowWidth/10));
 
 
-    //calling variable
-let mousePressed
+  }
+
+
+    
 }    
 
-function mousePressed () {
+function randomizer (){
 
-    //background restated so that the array text doesn't stack up on each other and "erases" it
-background(168,250,240);
-
-
-if (cats[0]){ //if something in array/if not in array 
+    if (cats[0]){ //if something in array/if not in array 
 
     // defining variable: int makes it a whole number, random(cats.length) chooses a random number within the array
 randomIndex = int(random(cats.length));
@@ -63,8 +68,21 @@ randomIndex = int(random(cats.length));
 
 } else { //if not in array the display
     textAlign (CENTER);
-text ('no more group of cats :(,',windowWidth/2,windowHeight/2);mousePressed () //nothing left game over 
+text ('no more group of cats :(',windowWidth/2,windowHeight/2);mousePressed () //nothing left game over 
 
 
 }
+
+
+}
+
+function mousePressed () {
+
+    //background restated so that the array text doesn't stack up on each other and "erases" it
+background(168,250,240);
+
+setTimeout(randomizer, 2000);
+
+
+
 }
