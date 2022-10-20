dc1fe732f5284cc4550493e58c1a0f9b7835ed3c
Hoda <hoda.afssari@sjsu.edu>
2022-10-19 20:52:10 -0700

    updating 2.10

--- 2c/2c.10/sketch.js
+++ 2c/2c.10/sketch.js
@@ -1,73 +1,5 @@
-// //Cats I love throughout my years and their owners 
 
-
-
-// //array
-// //global things
-// let cats = [
-//     {
-//         name: "Bella", 
-//         owner: "Alizeh"
-//     }, {
-//         name: "Zuko", 
-//         owner: "Sareh"
-//     }, {
-//         name: "Whisker", 
-//         owner: "Sareh"
-//     }, {
-//         name: "Cookie", 
-//         owner: "Maryam"
-//     }, {
-//         name: "Meowmeow", 
-//         owner: "Afssari"
-//     }, {
-//         name: "Meowmeow's triplets", 
-//         owner: "Afssari" 
-//     }];
-    
-
-
-//      //calling variable
-//  let randomIndex;
-//  let animating = false;
-//  let imageCounter = 0
-//  let catimages = [];
-//  let button;
-
-
-//     function preload(){
-    
-//         for (let i = 0; i <= 5; i++){
-//             catimages[i] = loadImage(`assets/catimage_`+i+`.JPG`)
-    
-//         }
-//     }
-    
-//     //runs once and stays
-//     function setup () {
-//         createCanvas (windowWidth,windowHeight);
-//         imageMode(CENTER);
-//         background (168,250,240);
-//         textAlign (CENTER);
-//         textSize (windowWidth/27);
-//         frameRate(15);
-        
-
-//          //saying this so user knows to click to start randomizer
-    
-//         text ('which cat should I hang out with',windowWidth/2.5,windowHeight/2.5);
-        
-//         console.log(catimages);
-        
-        
-//         createButton("click to choose which cat to hang out with");
-//         button.mousePressed(buttonPressed);
-//         button.position(50,50);
-
-    
-//     }
-    
-    
+  
 //     function draw () { 
         
 //         if (animating == true){
@@ -151,11 +83,13 @@ let cats = [
     }];
     
 
+
  let randomIndex;
  let animating = false;
  let imageCounter = 0
  let catimages = [];
  let button;
+
 
 
     function preload(){
@@ -170,33 +104,51 @@ let cats = [
     function setup (){
         createCanvas(windowWidth,windowHeight);
         background(168,250,240);
-        button = createButton("click to choose which cat to hang out with");
-        button.position(50,50);
+      //  button = createButton("click to choose which cat to hang out with");
+      button = select("#randButton")
+        button.position(windowWidth/2.3,windowHeight/15);
         button.mousePressed(buttonPressed);
+        button.class(".randomizerButton")
+        button.style("padding", "10px");
+        button.style("background-color", "#a8faf0")
+      
+        
         imageMode(CENTER);
+        textSize (windowWidth/50);
+      //  textAlign (CENTER);
     
-        frameRate(10);
+        frameRate(20);
         console.log(catimages);
        // text ("which cat should I hang out with",windowWidth/2.5,windowHeight/2.5);
     }
     
 
     
-    
-    
-    
     function draw(){
         if (animating == true) {
             clear();
-            image(catimages[imageCounter],width/2,height/2);
+            image(catimages[i][imageCounter],width/2.5,height/2.5);
         
             if(imageCounter < catimages.name -1) {
             imageCounter++;
+            console.log(imageCounter);
             } else {
             imageCounter = 0;
                 }
             }
         }
+
+
+            
+//         if (imageCounter < catimage.name - 1){
+//             imageCounter++;
+//             console.log(imageCounter);
+//         } else {
+//             imageCounter = 0;
+//         }
+//     }
+//     }
+    
     
         function randomizer (){
             animating = false;
@@ -218,5 +170,4 @@ let cats = [
     
     }
 
-    
 
